f849e0be39f242901f912c715d7e10a27d2bcd9e
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-01 17:06:16 -0700

    edit AllProjects component to render a table of skills accessed from firebase * need to resolve errors regarding HTML * need to refactor the axis call out of this component

--- lib/components/AllProjects.jsx
+++ lib/components/AllProjects.jsx
@@ -1,12 +1,45 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const axios = require('axios');
 
 class AllProjects extends React.Component{
+  constructor() {
+    super();
+    this.state = {projects: []}
+  }
+
+  componentDidMount() {
+    axios.get("https://project-manager-18532.firebaseio.com/projects.json?")
+         .then( (response) => {
+           console.log("response:", Object.keys(response.data))
+           this.setState({ projects: response.data});
+         })
+         .catch( (error) => {
+           console.log(error);
+           return error;
+         });
+  }
+
   render() {
+    let projects = this.state.projects.map((project) => {
+      return (
+        <table>
+          <tr>
+            <td>{project.name}</td>
+            <td>{project.description}</td>
+          </tr>
+        </table>
+      )
+    })
+
     return (
-      <div className="all-projects"> is this working </div>
+      <div className="all-projects">
+        {projects}
+      </div>
     )
   }
 };
 
 ReactDOM.render(<AllProjects />, document.getElementById('layout'))
+
+// export { AllProjects };
