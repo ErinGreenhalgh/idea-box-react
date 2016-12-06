8d1a84e10b0324a8c888651224a0be2febddfa86
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-06 13:30:06 -0700

    add functions so that form can submit data * need to add functionality to send data to db * need to add functionality so that form can submit data from more than one field

--- lib/components/NewProject.jsx
+++ lib/components/NewProject.jsx
@@ -3,11 +3,23 @@ import React from 'react';
 export default class NewProject extends React.Component {
   constructor() {
     super()
-    this.state = {formShowing: false}
+    this.state = {formShowing: false, value: ''}
     this.showForm = this.showForm.bind(this)
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   showForm () {
     this.setState({ formShowing: true })
+  }
+
+  handleNameChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //need to send this info to firebase
+    console.log("name submitted:", this.state.value)
+    event.preventDefault();
   }
 
   render() {
@@ -16,7 +28,11 @@ export default class NewProject extends React.Component {
     if (!formShowing) {
       htmlShown = <button onClick={ this.showForm } > + Add a Project </button>
     } else if (formShowing){
-      htmlShown = <h3> This is the new project form! </h3>
+      htmlShown =
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="name" placeholder='Project Name' value={this.state.value} onChange={this.handleNameChange}/>
+        <input type="submit" value="Add Project"/>
+      </form>
     }
 
     return (
