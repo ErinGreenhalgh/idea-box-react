f03b65f42c998ed9ad6363476abe62f0e90a42f3
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-07 12:10:43 -0700

    modify form so it can take in more than one input and submit those values * double-binding data * still getting a warning: “NewProject is changing an uncontrolled input of type text to be controlled.” need to look into this

--- lib/components/NewProject.jsx
+++ lib/components/NewProject.jsx
@@ -3,26 +3,36 @@ import React from 'react';
 export default class NewProject extends React.Component {
   constructor() {
     super()
-    this.state = {formShowing: false, value: ''}
-    this.showForm = this.showForm.bind(this)
+    // this.state = {formShowing: false, value: {name: "", description: ""}}
+    this.state = {formShowing: false, name: '', decription: ''}
+    this.showForm = this.showForm.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleDescChange = this.handleDescChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   showForm () {
     this.setState({ formShowing: true })
   }
 
   handleNameChange(event) {
-    this.setState({value: event.target.value});
+    console.log("event.target:", event.target)
+    this.setState({ name: event.target.value })
+  }
+
+  handleDescChange(event) {
+    this.setState({ description: event.target.value })
   }
 
   handleSubmit(event) {
     //need to send this info to firebase
-    console.log("name submitted:", this.state.value)
+    console.log("name submitted:", this.state.name)
+    console.log("description submitted:", this.state.description)
     event.preventDefault();
   }
 
   render() {
+    console.log("State.name:", this.state.name)
+    console.log("State.desc:", this.state.description)
     let formShowing = this.state.formShowing;
     let htmlShown = null;
     if (!formShowing) {
@@ -30,7 +40,8 @@ export default class NewProject extends React.Component {
     } else if (formShowing){
       htmlShown =
       <form onSubmit={this.handleSubmit}>
-        <input type="text" name="name" placeholder='Project Name' value={this.state.value} onChange={this.handleNameChange}/>
+        <input type="text" name="name" placeholder='Project Name' value={this.state.name} onChange={this.handleNameChange}/>
+        <input type="text" name="description" placeholder='Project Description' value={this.state.description} onChange={this.handleDescChange}/>
         <input type="submit" value="Add Project"/>
       </form>
     }
