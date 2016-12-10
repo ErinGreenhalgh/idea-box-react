81d81c859e35ccd085a1b2bcd054369099233a58
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-10 11:21:52 -0700

    refactor project state into Body from AllProjects * Body passes projects as props to AllProjects * probably need to refactor how Body makes the db call; this is messy right now

--- lib/components/AllProjects.jsx
+++ lib/components/AllProjects.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import 'firebase/app'
-import 'firebase/database'
-import firebase from 'firebase';
 
 export default class AllProjects extends React.Component{
-  constructor() {
-    super();
-    this.state = { projects: [] }
-    let app = firebase.initializeApp({ databaseURL: "https://project-manager-18532.firebaseio.com"});
-    let db = firebase.database();
-    this.ref = db.ref("projects")
-  }
-
-  componentDidMount() {
-    this.ref.on("value", (dataSnapshot) => {
-      this.setState({projects: dataSnapshot.val()})
-    })
-  }
 
   render() {
-    let projectObject = this.state.projects
+    let projectObject = this.props.projects
     let projects = [];
     for (var key in projectObject) {
       if(projectObject.hasOwnProperty(key)) {
@@ -31,7 +15,6 @@ export default class AllProjects extends React.Component{
         projects.push(html)
       }
     }
-    console.log("result:", projects)
 
     return (
       <div className="all-projects">
--- lib/components/Body.jsx
+++ lib/components/Body.jsx
@@ -2,13 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import AllProjects from './AllProjects';
 import NewProject from './NewProject';
+import 'firebase/app'
+import 'firebase/database'
+import firebase from 'firebase';
 
 class Body extends React.Component {
+  constructor() {
+    super()
+    let app = firebase.initializeApp({ databaseURL: "https://project-manager-18532.firebaseio.com"});
+    let db = firebase.database();
+    this.ref = db.ref("projects")
+    let projects = this.ref.on("value", (dataSnapshot) => {
+      this.setState({projects: dataSnapshot.val()})
+    })
+    this.state = {projects: projects, formShowing: false, name: "", description: ""}
+
+  }
+
+  // componentDidMount() {
+  //   this.ref.on("value", (dataSnapshot) => {
+  //     this.setState({projects: dataSnapshot.val()})
+  //   })
+  // }
+
   render() {
     return (
       <div>
         <NewProject />
-        <AllProjects />
+        <AllProjects projects={this.state.projects}/>
       </div>
     )
   }
