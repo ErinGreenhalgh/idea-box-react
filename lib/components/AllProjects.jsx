d7999daff9179f6d0bb9e801e77f59383abb5c97
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-10 15:01:31 -0700

    create a ProjectDetail component that renders a project once it has been selected from the AllProjects table * currently AllProjects is holding state of activeProject, and then contains a function on click of the given <tr> that updates the project statuses correctly and sets the state to the correct project * things are working now but needs a significant refactor

--- lib/components/AllProjects.jsx
+++ lib/components/AllProjects.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import ProjectDetail from './ProjectDetail'
+import firebase from 'firebase';
 
 export default class AllProjects extends React.Component{
+  constructor() {
+    super()
+    this.state = { activeProject: null }
+    this.selectActive = this.selectActive.bind(this)
+  }
+
+  selectActive(event) {
+    let projectId = event.target.id
+    let ref = firebase.database().ref("projects/")
+    let projects = {}
+    ref.on("value", (snapshot) => {
+      Object.assign(projects, snapshot.val())
+    })
+    for ( var key in projects) {
+      if (projects.hasOwnProperty(key)) {
+        if (key !== projectId) {
+          firebase.database().ref('projects/' + key).update({ active: false })
+        } else if (key === projectId) {
+          firebase.database().ref('projects/' + projectId).update({ active: true })
+        }
+      }
+    }
+    let project = {}
+    let projRef = firebase.database().ref("projects/" + projectId)
+    projRef.on("value", (snapshot) => {
+      Object.assign(project, snapshot.val())
+    })
+    this.setState({activeProject: project})
+  }
 
   render() {
     let projectObject = this.props.projects
@@ -8,9 +39,9 @@ export default class AllProjects extends React.Component{
     for (var key in projectObject) {
       if(projectObject.hasOwnProperty(key)) {
         let html =
-        <tr key={key} onClick={this.showProject}>
-          <td>{projectObject[key].name}</td>
-          <td>{projectObject[key].description}</td>
+        <tr key={key} onClick={this.selectActive}>
+          <td id={key}>{projectObject[key].name}</td>
+          <td id={key}>{projectObject[key].description}</td>
         </tr>
         projects.push(html)
       }
@@ -30,7 +61,7 @@ export default class AllProjects extends React.Component{
           </table>
         </section>
         <section className='project-detail'>
-          <ProjectDetail />
+          <ProjectDetail project={this.state.activeProject}/>
         </section>
       </div>
     )
--- lib/components/ProjectDetail.jsx
+++ lib/components/ProjectDetail.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
 export default class ProjectDetail extends React.Component {
-  constructor() {
-    super()
-  }
 
   render() {
-    return(
-      <section className="project">
-        <h1>This is a project</h1>
-      </section>
-    )
+    console.log("project:", this.props.project)
+    if (this.props.project === null) {
+      return(
+        <div> No project selected</div>
+      )
+    } else {
+      return(
+        <section className="project-detail">
+          <table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <td>{this.props.project.name}</td>
+              </tr>
+              <tr>
+                <th>Description</th>
+                <td>{this.props.project.description} </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      )
+    }
   }
 }
