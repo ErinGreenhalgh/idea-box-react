4cac1eedd2d87cc5fbeff9c536c1bcdfbac559a7
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-09 20:35:47 -0700

    begin functionality to show one project * create a ProjectDetail component * begin logic for rendering project detail from AllProjects

--- lib/components/AllProjects.jsx
+++ lib/components/AllProjects.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'firebase/app'
 import 'firebase/database'
 import firebase from 'firebase';
+import ProjectDetail from './ProjectDetail'
 
 export default class AllProjects extends React.Component{
   constructor() {
@@ -10,6 +11,12 @@ export default class AllProjects extends React.Component{
     let app = firebase.initializeApp({ databaseURL: "https://project-manager-18532.firebaseio.com"});
     let db = firebase.database();
     this.ref = db.ref("projects")
+  }
+
+  showProject() {
+    //project
+    console.log("showing project")
+    // this.setState({projectShowing: true})
   }
 
   componentDidMount() {
@@ -24,26 +31,30 @@ export default class AllProjects extends React.Component{
     for (var key in projectObject) {
       if(projectObject.hasOwnProperty(key)) {
         let html =
-        <tr key={key}>
+        <tr key={key} onClick={this.showProject}>
           <td>{projectObject[key].name}</td>
           <td>{projectObject[key].description}</td>
         </tr>
         projects.push(html)
       }
     }
-    console.log("result:", projects)
 
     return (
-      <div className="all-projects">
-        <table>
-          <tbody>
-            <tr>
-              <th>Project Name</th>
-              <th>Description</th>
-            </tr>
-            {projects}
-          </tbody>
-        </table>
+      <div>
+        <section className="all-projects">
+          <table>
+            <tbody>
+              <tr>
+                <th>Project Name</th>
+                <th>Description</th>
+              </tr>
+              {projects}
+            </tbody>
+          </table>
+        </section>
+        <section className='project-detail'>
+          <ProjectDetail />
+        </section>
       </div>
     )
   }
---
+++ lib/components/ProjectDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ProjectDetail extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(
+      <section className="project">
+        <h1>This is a project</h1>
+      </section>
+    )
+  }
+}
