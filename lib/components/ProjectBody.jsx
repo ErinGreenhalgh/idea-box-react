4e23b9a2c969e3314717b0925131269587ff9245
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-26 11:20:48 -0700

    create the bones of a container component that will contain the projects table, the project detail, and new project form as presentation components

---
+++ lib/components/ProjectBody.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ProjectBody extends React.Component {
+  constructor(state, props) {
+    //sets initial state
+    //binds functions that need binding
+  }
+
+  //child functions called by render to be defined here
+
+  render() {
+    //should render a child component and not contain any markup
+    return()
+  }
+}
+
+//proptypes and validations (need to do more research on this)
+//reduc connect and mapStateToDispatch and mapStateToProps
