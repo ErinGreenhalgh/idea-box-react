7ae592cd782889134b006b9b845b333789fc47eb
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-27 10:57:30 -0700

    wire up NewProjectManager to dispatch the createProject action in a createNewProject function * passes this function as props to the NewProjectForm to handle the submit * uncertain if the map state to props function is needed here…

--- lib/components/NewProjectManager.jsx
+++ lib/components/NewProjectManager.jsx
@@ -1,35 +1,50 @@
 import React, { PropTypes } from 'react';
 import NewProjectForm from './NewProjectForm';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as projectActions from '../actions/projectActions';
 
 class NewProjectManager extends React.Component {
   constructor(){
     super();
     this.state = { project: {} }
     this.updateProjectFields = this.updateProjectFields.bind(this);
+    this.createNewProject = this.createNewProject.bind(this);
   }
 
   updateProjectFields(event) {
     const field = event.target.name;
     let project = this.state.project;
     project[field] = event.target.value;
-    return this.setState({ project: project });
+  }
+
+  createNewProject(event){
+    event.preventDefault();
+    this.props.actions.createProject(this.state.project);
   }
 
   render() {
     return(
       <NewProjectForm project={this.state.project}
-                      onChange={this.updateProjectFields}/>
+                      onChange={this.updateProjectFields}
+                      handleSubmit={this.createNewProject}/>
     )
   }
 }
 
 
-// function mapStateToProps(state, ownProps) {
-//   return {
-//     project: state.project
-//   }
-// }
+function mapStateToProps(state, ownProps) {
+  return {
+    project: state.project
+  }
+}
 
-// export default connect(mapStateToProps)(NewProjectManager);
-export default NewProjectManager;
+function mapDispatchToProps(dispatch){
+  return{
+    actions: bindActionCreators(projectActions, dispatch)
+    //this component doesn't need all the actions, consider refactoring
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(NewProjectManager);
+// export default NewProjectManager;
