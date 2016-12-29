90f43f602f1341d7a84342d24cc8abfe870d70c3
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-29 16:26:46 -0700

    add action, reducer, and function in ProjectBody to set activeProject to null to close the projectDetail view

--- lib/components/ProjectBody.jsx
+++ lib/components/ProjectBody.jsx
@@ -11,12 +11,16 @@ class ProjectBody extends React.Component {
   constructor() {
     super();
     this.selectActive = this.selectActive.bind(this);
-    // this.deactivateProject = this.deactivateProject.bind(this);
+    this.deactivateProject = this.deactivateProject.bind(this);
   }
 
   selectActive(event) {
     let id = event.target.id
     this.props.actions.getProject(id);
+  }
+
+  deactivateProject(){
+    this.props.actions.clearProject();
   }
 
   render() {
@@ -25,9 +29,9 @@ class ProjectBody extends React.Component {
         <div className='project-area'>
           <ProjectNav />
           <ProjectsTable projects={this.props.projects}
-            selectActive={this.selectActive}/>
+                         selectActive={this.selectActive}/>
           <ProjectDetail project={this.props.activeProject}
-                         />
+                         deactivateProject={this.deactivateProject}/>
         </div>
       )
     } else {
