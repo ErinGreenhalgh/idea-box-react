152f03141993990927145b299957dfc67707ea07
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-30 11:31:01 -0700

    change back to hard-coded <tr>s for now; * if we wanted the user to be able to manipulate the project template, we would need to address the problem of ordering of project attributes; for now we can leave it hard-coded

--- lib/components/ProjectDetail.jsx
+++ lib/components/ProjectDetail.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import DetailRow from './DetailRow';
 
 const ProjectDetail = ({ project, deactivateProject, handleDelete }) => {
-  let detailRows = []
-  for (var key in project) {
-    if (project.hasOwnProperty(key) && key != "id"){
-      let html = <tr key={key}>
-        <th>{key}</th>
-        <td>{project[key]}</td>
-      </tr>
-      detailRows.push(html)
-    }
-  }
-
   return(
     <section className="project-detail overlay">
       <article className='detail-content'>
         <h1 id='close-detail' onClick={deactivateProject}> X </h1>
         <table id='detail-table'>
           <tbody>
-            {detailRows}
+            <tr>
+              <th>Name</th>
+              <td>{project.name}</td>
+            </tr>
+            <tr>
+              <th>Description</th>
+              <td>{project.description}</td>
+            </tr>
+            <tr>
+              <th>Phase</th>
+              <td>{project.phase}</td>
+            </tr>
+            <tr>
+              <th>Due Date</th>
+              <td>{project.due_date}</td>
+            </tr>
           </tbody>
         </table>
         <button id={project.id}
