5c33505b60b7f1b1aca4db09a370187c84e3d38f
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-30 11:22:31 -0700

    refactor ProjectDetail to iterate over the keys in the project and create a row with that data *this is much less hard-coded, but now we have the problem of wanting to order the attributes in a specific way

--- lib/components/ProjectDetail.jsx
+++ lib/components/ProjectDetail.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import DetailRow from './DetailRow';
 
 const ProjectDetail = ({ project, deactivateProject, handleDelete }) => {
+  let detailRows = []
+  for (var key in project) {
+    if (project.hasOwnProperty(key) && key != "id"){
+      let html = <tr key={key}>
+        <th>{key}</th>
+        <td>{project[key]}</td>
+      </tr>
+      detailRows.push(html)
+    }
+  }
+
   return(
     <section className="project-detail overlay">
       <article className='detail-content'>
         <h1 id='close-detail' onClick={deactivateProject}> X </h1>
         <table id='detail-table'>
           <tbody>
-            <tr>
-              <th>Name</th>
-              <td id='project-name'>{project.name}</td>
-            </tr>
-            <tr>
-              <th>Description</th>
-              <td id='project-description' >{project.description} </td>
-            </tr>
+            {detailRows}
           </tbody>
         </table>
         <button id={project.id}
