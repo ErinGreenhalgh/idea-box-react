166901c6b1c1155c4d6b3483a2f0953ec7673672
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-01-01 18:02:01 -0700

    get ProjectDetail back to where it was, with just a table showing the project information

--- lib/components/ProjectDetail.jsx
+++ lib/components/ProjectDetail.jsx
@@ -6,50 +6,26 @@ const ProjectDetail = ({ project, handleClick, handleSubmit, handleChange, handl
     <section className="project-detail overlay">
       <article className='detail-content'>
         <h1 className='close-detail' id={ project.id } onClick={ handleClick }> X </h1>
-        <form onSubmit={ handleSubmit }>
-          <table id='detail-table'>
-            <tbody>
-              <tr>
-                <th>Name</th>
-                <td>
-                  <input type='text'
-                         name="name"
-                         value={ project.name }
-                         onChange={ handleChange }></input>
-                </td>
-              </tr>
-              <tr>
-                <th>Description</th>
-                  <td>
-                    <input type='text'
-                           name="description"
-                           value={ project.description }
-                           onChange={ handleChange }></input>
-                  </td>
-              </tr>
-              <tr>
-                <th>Phase</th>
-                  <td>
-                    <input type='text'
-                           name="phase"
-                           value={ project.phase }
-                           onChange={ handleChange }></input>
-                  </td>
-              </tr>
-              <tr>
-                <th>Due Date</th>
-                  <td>
-                    <input type='text'
-                           name="due_date"
-                           value={ project.due_date }
-                           onChange={ handleChange }></input>
-                  </td>
-              </tr>
-            </tbody>
-          </table>
-          <input type="submit"
-                 value="Create / Update"></input>
-        </form>
+        <table id='detail-table'>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <td>{ project.name }</td>
+            </tr>
+            <tr>
+              <th>Description</th>
+                <td>{ project.description }</td>
+            </tr>
+            <tr>
+              <th>Phase</th>
+                <td>{ project.phase }</td>
+            </tr>
+            <tr>
+              <th>Due Date</th>
+                <td> { project.due_date }</td>
+            </tr>
+          </tbody>
+        </table>
         <button id={project.id} onClick={handleDelete}>Delete</button>
       </article>
     </section>
