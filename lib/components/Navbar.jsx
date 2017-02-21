61b180dedf13b43265b824b47a6fc5035d7c5d64
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-02-20 17:51:32 -0700

    create a dumb component for the navbar that will render any buttons that belong in the nav

---
+++ lib/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddProjectButton from './AddProjectButton';
+
+const Navbar = ({props}) => {
+  return(
+    <div>
+      <AddProjectButton />
+    </div>
+  )
+}
+
+export default Navbar;
