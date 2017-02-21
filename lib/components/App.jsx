989590938c6339501fe429a08244705ea85e957f
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-02-21 13:56:12 -0700

    working with the form manager component: * render this component from App instead of the NavbarManager; want to isolate the workings of the form from the display of the form

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -7,7 +7,7 @@ import * as projectActions from '../actions/projectActions';
 import ProjectNav from './ProjectNav';
 import { getProject } from '../database/accessProjects';
 import ProjectDetail from './ProjectDetail';
-import NavbarManager from './NavbarManager';
+import NewProjectManager from './NewProjectManager';
 
 class App extends React.Component {
   constructor(props, context) {
@@ -38,7 +38,7 @@ class App extends React.Component {
     if (this.props.activeProject) {
       return(
         <div className='project-area'>
-          <NavbarManager />
+          <NewProjectManager />
           <ProjectsTable projects={this.props.projects}
                          selectActive={this.selectActive}/>
           <ProjectDetail project={this.props.activeProject}
@@ -50,7 +50,7 @@ class App extends React.Component {
     } else {
       return(
         <div className='project-area'>
-          <NavbarManager />
+          <NewProjectManager />
           <ProjectsTable projects={this.props.projects}
                          selectActive={this.selectActive}/>
         </div>
