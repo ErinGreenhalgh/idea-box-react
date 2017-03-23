1b91cbfcceb3eb9cec0610e45803affda373454b
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-03-23 16:05:45 -0600

    tweak test for project actions:
    * nock out the firebase url
    * change expected actions to just LOAD_PROJECTS_SUCCESS, 
    changing plural types to singular (lines 31-33)

--- test/actions/projectActions.spec.js
+++ test/actions/projectActions.spec.js
@@ -14,22 +14,21 @@ describe('async project actions', () => {
     nock.cleanAll();
   });
 
-  it('created LOAD_PROJECT_SUCCESS when all projects are successfully loaded', () => {
+  it('creates LOAD_PROJECT_SUCCESS when all projects are successfully loaded', () => {
     let projects = {
       1: {name: "first project",  description: "it's great", phase: "0", due_date: "12-13-17"},
       2: {name: "second project", description: "also great", phase: "0", due_date: "12-15-17"},
       3: {name: "third project",  description: "greatest",   phase: "0", due_date: "12-17-17"}
     }
 
-    //maybe I want a mock API and to test actual firebase API calls elsewhere
-    nock('http://example.com/')
+    nock('https://project-manager-18532.firebaseio.com/')
       .get('/projects')
       .reply(200, { body: { projects: projects }
     })
 
+
     const expectedActions = [
-      { types: types.LOAD_PROJECTS },
-      { types: types.LOAD_PROJECTS_SUCCESS, body: { projects: projects} }
+      { type: types.LOAD_PROJECTS_SUCCESS, body: { projects: projects} }
     ]
 
     const store = mockStore({ projects: {} });
